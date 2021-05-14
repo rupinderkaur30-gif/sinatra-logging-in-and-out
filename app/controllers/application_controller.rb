3f5b9a2906650bfb7173212078515e6c518500f7
rupinderkaur30-gif <rupi063094@gmail.com>
2021-05-14 16:50:26 -0400

    commit tree 23978685e434422fd853fe8c21eb0cfdceebe22c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    user = User.find_by(username: params["username"], password: params["password"])
-    if user
+    @user = User.find_by(username: params["username"] )
+    if @user && @user.password == params["password"]
+ 
       session[:user_id] = @user.id
       redirect to '/account'
     else
